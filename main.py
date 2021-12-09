df8e5177421ae566b72959d54043b2fbe2761111
Mercurio005 <mercurio117@hotmail.com>
2021-12-09 16:16:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,36 @@ from sklearn.decomposition import PCA
 from tensorflow import keras
 import numpy as np
 
+class ModeDL:
+  def __init__(self, path, bandsNames, imageSize, epochs):
+    self.__path = path
+    self.__bands = bandsNames
+    self.__size = imageSize
+    self.__epochs = epochs
+    self.__model = None
+    self.__callbacks = callbacks = [
+               keras.callbacks.ModelCheckpoint("agaves_segmentation.h5", save_best_only=True)]
+    
+  def train(self):
+    X, y = LoadData(self.__path, self.__bands, self.__size)
+    X_train, X_valid, y_train, y_valid = train_test_split(np.array(X), np.array(y), test_size = 0.3)
+    self.__model = DeepLearning.U_Net(X_train[0].shape, len(np.unique(y_train)), X_train)
+    self.__model.fit(X_train, y_train, epochs=epochs, validation_data=(X_valid, y_valid), callbacks=callbacks)
+    
+class ModeML:
+  def __init__(self, path, bandsNames, imageSize, method):
+    self.__path = path
+    self.__bands = bandsNames
+    self.__size = imageSize
+    self.__model = ML.MLModel(method)
+    self.__pca = None
+    
+  def train(self):
+    X, y = LoadData(self.__path, self.__bands, self.__size)
+    X_train, X_valid, y_train, y_valid = train_test_split(np.array(X), np.array(y), test_size = 0.3)
+    Xnew, ynew, self.__pca = transformData(X_train, y_train)
+    self.__model.fit(Xnew, ynew)
+
 def DLMode(path, bandsNames, imageSize, epochs):
   X, y = LoadData(path, bandsNames, imageSize)
   X_train, X_valid, y_train, y_valid = train_test_split(np.array(X), np.array(y), test_size = 0.3)
@@ -43,4 +73,4 @@ def transformData(X_train, y_train):
 
   x_train_pca = pca.fit_transform(newX_train)
 
-  return x_train_pca, newy_train
+  return x_train_pca, newy_train, pca
