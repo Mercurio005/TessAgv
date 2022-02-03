da99192212631d24b1081ec2f0d1c5a10360b0e1
Mercurio005 <mercurio117@hotmail.com>
2022-02-03 16:31:06 -0600

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,9 @@ class ModeDL:
     self.__tiles = None
     self.__history = None
     self.__callbacks = callbacks = [
-               keras.callbacks.ModelCheckpoint("agaves_segmentation.h5", save_best_only=True)]
+               keras.callbacks.ModelCheckpoint("agaves_segmentation.h5", save_best_only=True),
+               keras.callbacks.EarlyStopping(monitor='val_accuracy', patience=5)
+               ]
     
   def train(self):
     X, y = LoadData(self.__path, self.__bands, self.__size)
@@ -56,7 +58,7 @@ class ModeDL:
     else:
       print("Model not implemented")
       return 0
-    self.__model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy")  #  keras.optimizers.Adam(learning_rate=1e-4)
+    self.__model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy", metrics=['accuracy'])  #  keras.optimizers.Adam(learning_rate=1e-4)
     
 class ModeML:
   def __init__(self, path, bandsNames, imageSize, method):
@@ -66,31 +68,22 @@ class ModeML:
     self.__model = ML.MLModel(method)
     self.__pca = None
     self.__tiles = None
-    self.__history = None
     
   def train(self):
     X, y = LoadData(self.__path, self.__bands, self.__size)
     X_train, X_valid, y_train, y_valid = train_test_split(np.array(X), np.array(y), test_size = 0.3)
     Xnew, ynew = self.__transformData(X_train, y_train)
-    self.__history = self.__model.fit(Xnew, ynew)
+    self.__model.fit(Xnew, ynew)
     
   def predict(self, path):
-    if self.__history is not None:
-      X, self.__tiles = LoadFile(path, self.__bands, self.__size)
-      newX = X.reshape(X.shape[0], -1)
-      newX_pca = self.__pca.transform(newX)
-      predicted0 = self.__model.predict(newX_pca)
-      predicted = predicted0.reshape((predicted0.shape[0],) + self.__size)
-      listY = concatTiles(predicted, self.__tiles)
-      return listY
-    print("Model not trained")
+    X, self.__tiles = LoadFile(path, self.__bands, self.__size)
+    newX = X.reshape(X.shape[0], -1)
+    newX_pca = self.__pca.transform(newX)
+    predicted0 = self.__model.predict(newX_pca)
+    predicted = predicted0.reshape((predicted0.shape[0],) + self.__size)
+    listY = concatTiles(predicted, self.__tiles)
+    return listY
   
-  def plot_history(self):
-    if self.__history is not None:
-      show_history(self.__history)
-    else:
-      print("Model not trained")
-    
   def __transformData(self, X_train, y_train):
     newX_train = X_train.reshape(X_train.shape[0], -1)
     newy_train = y_train.reshape(y_train.shape[0], -1)
