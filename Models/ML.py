d1bcf80e13daf480bf2b877a09362fed581fc78d
Mercurio005 <mercurio117@hotmail.com>
2022-02-03 20:34:40 -0600

    Update ML.py

--- Models/ML.py
+++ Models/ML.py
@@ -5,7 +5,7 @@ from sklearn.tree import DecisionTreeClassifier
 
 def MLModel(modelSTR):
   dictML = {"KNN": KNeighborsClassifier(),
-            "R-Forest": RandomForestClassifier(),
+            "R-Forest": RandomForestClassifier(n_jobs=1, max_depth=10),
             "D-Tree": DecisionTreeClassifier()}
   model = dictML.get(modelSTR)
   return model
